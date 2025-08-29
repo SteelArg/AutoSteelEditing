e632221151d4983c6f2e5c328808e1c23731d84d
Andrew <andrusin99@gmail.com>
2025-08-30 01:45:46 +0300

    Added NullTimeValue

---
+++ Core/DataTypes/TimeValues/NullTimeValue.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.DataTypes.TimeValues;
+
+public class NullTimeValue : ITimeValue {
+
+    public float Seconds => 0;
+
+    public NullTimeValue() {
+        
+    }
+
+    public override string ToString() {
+        return "NullTimeValue";
+    }
+
+}
