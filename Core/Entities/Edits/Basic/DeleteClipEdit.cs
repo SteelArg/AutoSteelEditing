a7b83e28c99f2b630c46d205ceb552b95c2ab7a8
Andrew <andrusin99@gmail.com>
2025-11-11 20:26:59 +0200

    Added a delete clip edit

---
+++ Core/Entities/Edits/Basic/DeleteClipEdit.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities.Clips;
+
+namespace Core.Entities.Edits.Basic;
+
+public class DeleteClipEdit : IClipEdit {
+
+    public void Apply(IClip clip) {
+        if (clip.Timeline == null) {
+            throw new InvalidOperationException("Can not delete a clip from null timeline.");
+        }
+
+        clip.Timeline.RemoveClip(clip);
+    }
+
+}
